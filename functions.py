521709652fa00a669aef7553bb5ea4c9a449eb80
FoxGreatNews <jpmacdo@clemson.edu>
2022-08-23 15:32:32 -0400

    cleaning

--- functions.py
+++ functions.py
@@ -10,7 +10,6 @@ def mkDir(name):
     name = name.replace('/','_')
 
     path = os.getcwd() + str(name)
-    # print (name)
     try:
         path = os.path.join(str(name))
         os.mkdir(path)
@@ -18,10 +17,8 @@ def mkDir(name):
     return path
 
 def getAbstract (url,dir):
-    # Making a GET request
     r = requests.get(url)
 
-    # Parsing the HTML
     soup = BeautifulSoup(r.content, 'html.parser')
     lines = soup.find_all('p')
 
@@ -33,36 +30,9 @@ def getAbstract (url,dir):
             bodyContent = line.text
             charCount = len(line.text)
 
-    # print (bodyContent)
     text_file = open(dir+"/abstract.txt", "w")
     n = text_file.write(bodyContent)
     text_file.close()
-
-
-def download_pdf(url, file_name, dir, headers):
-
-    # Send GET request
-    response = requests.get(url, headers=headers)
-
-    # Save the PDF
-    if response.status_code == 200:
-        with open(dir + '/' +file_name, "wb") as f:
-            f.write(response.content)
-    else: pass
-        # print(response.status_code)
-
-
-def savePdf(url,dir):
-    # Define HTTP Headers
-    headers = {
-        "User-Agent": "Chrome/51.0.2704.103",
-    }
-
-    # Define image file name
-    file_name = "file1.pdf"
-
-    # Download image
-    download_pdf(url, file_name, dir, headers)
 
 def getLink (start, text) :
     closest = 0
@@ -75,35 +45,26 @@ def getLink (start, text) :
                 closest = index
                 break
             
-    # print (text[closest+6])
     endofUrl = text[closest+6:].find('"')
-    # print (closest+endofUrl +6)
-    # print (text[closest+6:endofUrl + closest+6])
     return text[closest+6:endofUrl + closest+6]
     
     
 def querySearch (query) :
+    #another user option
     # Mozilla/5.0 (Macintosh; Intel Mac OS X 0_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9
     headers = {'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Safari/605.1.15 Version/13.0.4'}
+    #Tried using my zero-tier proxies
     # proxies = {"http": "http://172.29.15.137:9993","https": "https://172.29.15.137:16"}
 
-
-    # titl = 'Designing human-autonomy teaming experiments through reinforcement learning'
-
-    # 'Fostering Human-Agent Team Leadership by Leveraging Human Teaming Principles'
     titl = query
     query = query.replace(' ','+')
     query+= '&btnG'
     url = 'https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q=' + query
     response=requests.get(url,headers=headers)
-    # print (response)
+
     soup=BeautifulSoup(response.content,'lxml')
 
-    # print(soup.select('[data-lid]'))
-
     contents = str(soup.select('[data-lid]'))
-
-    # print (contents.find(titl))
 
     return getLink(contents.find(titl),contents)
 
@@ -111,3 +72,29 @@ def saveText (fileName,dir,content) :
     text_file = open(dir + '/' + fileName, "w")
     n = text_file.write(content)
     text_file.close()
+
+
+# def download_pdf(url, file_name, dir, headers):
+
+#     # Send GET request
+#     response = requests.get(url, headers=headers)
+
+#     # Save the PDF
+#     if response.status_code == 200:
+#         with open(dir + '/' +file_name, "wb") as f:
+#             f.write(response.content)
+#     else: pass
+#         # print(response.status_code)
+
+
+# def savePdf(url,dir):
+#     # Define HTTP Headers
+#     headers = {
+#         "User-Agent": "Chrome/51.0.2704.103",
+#     }
+
+#     # Define image file name
+#     file_name = "file1.pdf"
+
+#     # Download image
+#     download_pdf(url, file_name, dir, headers)
